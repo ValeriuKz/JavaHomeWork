86559716144729986e93a920526363cd534e6c0c
Valeriu Kozyrenko <valerij.kozyrenko@privatbank.ua>
2021-10-15 03:56:49 +0300

    init commit

---
+++ src/com/pb/kozyrenko/hw2/Calculator.java
@@ -0,0 +1,48 @@
+package com.pb.kozyrenko.hw2;
+
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+        double operand1;
+        double operand2;
+        String sign;
+        double result;
+
+
+        System.out.print("Введите первое числовое значение : ");
+        operand1 = scan.nextDouble();
+
+        System.out.print("Введите второе числовое значение : ");
+        operand2 = scan.nextDouble();
+
+        System.out.print("Введите тип выполняемой операции (+, -, *, /): ");
+        sign = scan.next();
+
+        switch(sign) {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "/":
+                if (operand1!=0) {
+                    if (operand2!=0){
+                result = operand1 / operand2;
+                break;
+                   }}
+        default:
+                System.out.print("Ошибка, введите корретные данные");
+                return;
+        }
+        System.out.print("\nРезультат:\n");
+        System.out.print(operand1 + " " + sign + " " + operand2 + " = " + result);
+    }
+}
